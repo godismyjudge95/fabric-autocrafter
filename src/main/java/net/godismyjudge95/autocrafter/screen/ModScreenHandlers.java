c0ec09cfb8f0be878e4d2439f81ce4602151395a
Daniel Weaver <godismyjudge95@gmail.com>
2023-04-30 23:19:55 -0400

    Fixing registring screen handler on server

--- src/main/java/net/godismyjudge95/autocrafter/screen/ModScreenHandlers.java
+++ src/main/java/net/godismyjudge95/autocrafter/screen/ModScreenHandlers.java
@@ -1,11 +1,17 @@
 package net.godismyjudge95.autocrafter.screen;
 
+import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
+import net.godismyjudge95.autocrafter.AutocrafterMod;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.screen.ScreenHandlerType;
 
 public class ModScreenHandlers {
-    public static ScreenHandlerType<AutocrafterScreenHandler> AUTOCRAFTER_SCREEN_HANDLER;
+    @SuppressWarnings("deprecation")
+    public static ScreenHandlerType<AutocrafterScreenHandler> AUTOCRAFTER_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(AutocrafterMod.BLOCK_ID, AutocrafterScreenHandler::new);
 
     public static void registerScreenHandlers() {
-        AUTOCRAFTER_SCREEN_HANDLER = new ScreenHandlerType<>(AutocrafterScreenHandler::new);
+
+        AutocrafterMod.LOGGER.info("Registering ModScreenHandlers for " + AutocrafterMod.MOD_ID);
     }
 }
